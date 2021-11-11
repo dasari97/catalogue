8b42c656c0a7d079f8b12dfe8d0e9ad2b1304cc7
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-11 11:24:09 +0000

    adding new file

--- Terraform-Immutable/main.tf
+++ Terraform-Immutable/main.tf
@@ -1,10 +1,10 @@
 module "catalogue" {
-    source = "git::https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/immutable//APP-module"
+    source = "git::https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/immutable//App-module"
     env = var.env
     instance_type = var.instance_type
     component   = "catalogue"
     port        = 8080
-#    is_internal = "true"
-#    priority    = 200
+    is_internal = "true"
+    priority    = 200
     APP_VERSION = var.APP_VERSION
 }
